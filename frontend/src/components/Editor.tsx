3dcacea7e8fceed7bedb3000e5dbf6b5786f7ac2
Brian Duff <brian@dufflin.com>
2023-11-26 23:44:12 -0800

    neater css

--- frontend/src/components/Editor.tsx
+++ frontend/src/components/Editor.tsx
@@ -3,7 +3,12 @@ import { MDXEditor, headingsPlugin, linkPlugin, listsPlugin, markdownShortcutPlu
 
 export function Editor() {
   return (
-  <div css={css`overflow: scroll; background: rgba(17, 20, 24, 0.3); color: #f6f7f9; box-shadow: 0 0 0 0 rgba(76, 144, 240, 0), 0 0 0 0 rgba(76, 144, 240, 0), 0 0 0 0 rgba(76, 144, 240, 0), inset 0 0 0 1px rgba(255, 255, 255, 0.2), inset 0 -1px 1px 0 rgba(255, 255, 255, 0.3); padding: 5px;`}>
+  <div css={css`
+      overflow: scroll;
+      background: rgba(17, 20, 24, 0.3);
+      color: #f6f7f9;
+      box-shadow: 0 0 0 0 rgba(76, 144, 240, 0), 0 0 0 0 rgba(76, 144, 240, 0), 0 0 0 0 rgba(76, 144, 240, 0), inset 0 0 0 1px rgba(255, 255, 255, 0.2), inset 0 -1px 1px 0 rgba(255, 255, 255, 0.3);
+      padding: 5px;`}>
     <MDXEditor
       plugins={[
           headingsPlugin(),
