80e3dfa537d92df18ce6a05e290af55a3747bf26
Brian Duff <brian@dufflin.com>
2023-12-11 22:08:02 -0800

    Update track for dev

--- frontend/src/EditMemo.tsx
+++ frontend/src/EditMemo.tsx
@@ -3,11 +3,12 @@ import { css } from "@emotion/react";
 import { Editor } from "./components/Editor";
 
 export function EditMemo() {
+  let track = import.meta.env.TRACK || undefined;
   return (
     <>
       <Navbar fixedToTop={true}>
         <Navbar.Group>
-          <Navbar.Heading>Foobar - auto updated version!!</Navbar.Heading>
+          <Navbar.Heading>Memo{track && track}</Navbar.Heading>
         </Navbar.Group>
       </Navbar>
       <div css={css`margin-top: 50px;`}>
